ff69b932e8c7da7ba11f01dbb3b8a3955252d0ba
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2025-02-24 10:50:34 +0800

    西元2025年02月24日 (週一) 10時50分34秒 CST

--- app/temporal-file-arragnement/getFiles.js
+++ app/temporal-file-arragnement/getFiles.js
@@ -8,17 +8,17 @@ const stat = util.promisify(fs.stat);
 const readdir = util.promisify(fs.readdir);
 
 // 取得所有檔案（排除 bundle）
-async function getFiles(dir, excludeDir) {
+async function getFiles(dir, excludeDir, baseDir = '/input') {
     let files = [];
     const items = await readdir(dir, { withFileTypes: true });
 
     for (const item of items) {
         const fullPath = path.join(dir, item.name);
-        const relativePath = path.relative(__dirname, fullPath);
+        const relativePath = path.relative(baseDir, fullPath);
 
         if (item.isDirectory()) {
             if (item.name !== excludeDir) {
-                const subFiles = await getFiles(fullPath, excludeDir);
+                const subFiles = await getFiles(fullPath, excludeDir, baseDir);
                 files = files.concat(subFiles);
             }
         } else {
