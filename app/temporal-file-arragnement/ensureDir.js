78a1ba4135f4859eb098d369ff8996b2eaf96cfb
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2025-02-24 21:35:50 +0800

    西元2025年02月24日 (週一) 21時35分50秒 CST

--- app/temporal-file-arragnement/ensureDir.js
+++ app/temporal-file-arragnement/ensureDir.js
@@ -5,6 +5,7 @@ const mkdir = util.promisify(fs.mkdir);
 
 // 建立目標資料夾（如果不存在）
 async function ensureDir(dir) {
+    console.log({dir, e: fs.existsSync(dir)})
     if (!fs.existsSync(dir)) {
         await mkdir(dir, { recursive: true });
     }
