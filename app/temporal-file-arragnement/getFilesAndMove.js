90ba94898511a535b97284948cc58a1f2bd594dd
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2025-02-25 11:09:20 +0800

    西元2025年02月25日 (週二) 11時09分20秒 CST

--- app/temporal-file-arragnement/getFilesAndMove.js
+++ app/temporal-file-arragnement/getFilesAndMove.js
@@ -22,36 +22,44 @@ async function getFilesAndMove(dir, excludeDir, baseDir) {
     const items = await readdir(dir, { withFileTypes: true });
 
     for (const item of items) {
-        console.log(dir, item.name)
-        if (isExcluded(item.name)) {
-          continue
+        
+        try {
+            if (isExcluded(item.name)) {
+                continue
+              }
+      
+              const fullPath = path.join(dir, item.name);
+              const relativePath = path.relative(baseDir, fullPath);
+      
+              if (item.isDirectory()) {
+                  if (item.name !== excludeDir) {
+                      await getFilesAndMove(fullPath, excludeDir, baseDir);
+                      // const subFiles = await getFiles(fullPath, excludeDir, baseDir);
+                      // files = files.concat(subFiles);
+                  }
+              } else {
+                  const createdAt = await getFileCreationTime(fullPath);
+                  // files.push({ path: relativePath, createdAt });
+      
+                  const YYYY = createdAt.getFullYear();
+                  const MM = String(createdAt.getMonth() + 1).padStart(2, '0');
+                  const DD = String(createdAt.getDate()).padStart(2, '0');
+                  
+                  const baseFolder = path.join(baseTargetFolder, YYYY.toString(), MM, `${YYYY}${MM}${DD}`);
+      
+                  const sourcePath = fullPath
+                  const targetPath = path.join(baseFolder, relativePath);
+                  
+                  await ensureDir(path.dirname(targetPath));
+                  await rename(sourcePath, targetPath);
+              }
         }
-
-        const fullPath = path.join(dir, item.name);
-        const relativePath = path.relative(baseDir, fullPath);
-
-        if (item.isDirectory()) {
-            if (item.name !== excludeDir) {
-                await getFilesAndMove(fullPath, excludeDir, baseDir);
-                // const subFiles = await getFiles(fullPath, excludeDir, baseDir);
-                // files = files.concat(subFiles);
-            }
-        } else {
-            const createdAt = await getFileCreationTime(fullPath);
-            // files.push({ path: relativePath, createdAt });
-
-            const YYYY = createdAt.getFullYear();
-            const MM = String(createdAt.getMonth() + 1).padStart(2, '0');
-            const DD = String(createdAt.getDate()).padStart(2, '0');
-            
-            const baseFolder = path.join(baseTargetFolder, YYYY.toString(), MM, `${YYYY}${MM}${DD}`);
-
-            const sourcePath = fullPath
-            const targetPath = path.join(baseFolder, relativePath);
-            
-            await ensureDir(path.dirname(targetPath));
-            await rename(sourcePath, targetPath);
+        catch (e) {
+            console.log(dir, item.name)
+            console.error(`Error: ${e.message}`);
+            continue
         }
+        
     }
 
     // return files;
