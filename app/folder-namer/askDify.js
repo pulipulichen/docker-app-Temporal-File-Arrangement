3a231d7ba48de2b85a390eb95a3e3cd0da69e737
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2025-03-21 00:42:28 +0800

    西元2025年03月21日 (週五) 00時42分28秒 CST

--- app/folder-namer/askDify.js
+++ app/folder-namer/askDify.js
@@ -27,7 +27,10 @@ async function uploadFile(filePath, yek, user) {
       // const fileExtension = path.extname(filePath).slice(1).toLowerCase();
       const ext = path.extname(filePath).toLocaleLowerCase()
       let contentType = mime.lookup(ext)
-      console.log({contentType})
+      // console.log({contentType})
+      if (["application/msword"].inArray(contentType)) {
+        contentType = "application/octet-stream"
+      }
       if (contentType !== false) {
         formData.append('file', fileStream, {
           contentType: contentType,
